56aebb9a0c8eb9c5ee821f5e24f6bc49cca04fdb
philippannenko <philippannenko@gmail.com>
2016-01-28 20:44:20 -0500

    Add segment on next playable tiles.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var app = app || {};
 $(function () {
   'use strict';
   app.TILE_SEQ_NUM = 10;
+  app.SEGMENT_SEQ_NUM = 1;
   Array.prototype.clear = function () {
     this.splice(0, this.length);
   };
@@ -84,99 +85,86 @@ $(function () {
     two_face_adjacent_castle: {
       class: 'two-face-adjacent-castle',
       count: 5,
-      faces: {T: 'FFF', R: 'FFF', B: 'CCC', L: 'CCC'},
-      rotation: app.Rotation._0
+      faces: {T: 'FFF', R: 'FFF', B: 'CCC', L: 'CCC'}
     },
     one_face_castle_with_t_road: {
       class: 'one-face-castle-with-t-road', count: 3,
-      faces: {T: 'FRF', R: 'FRF', B: 'FRF', L: 'CCC'},
-      rotation: app.Rotation._0
+      faces: {T: 'FRF', R: 'FRF', B: 'FRF', L: 'CCC'}
     },
     one_face_opposite_separate_castles: {
       class: 'one-face-opposite-separate-castles', count: 3,
-      faces: {T: 'FFF', R: 'CCC', B: 'FFF', L: 'CCC'},
-      rotation: app.Rotation._0
+      faces: {T: 'FFF', R: 'CCC', B: 'FFF', L: 'CCC'}
     },
     cloister: {
       class: 'cloister ', count: 4,
-      faces: {T: 'FFF', R: 'FFF', B: 'FFF', L: 'FFF'},
-      rotation: app.Rotation._0
+      faces: {T: 'FFF', R: 'FFF', B: 'FFF', L: 'FFF'}
     },
     cloister_with_road: {
       class: 'cloister-with-road', count: 2,
-      faces: {T: 'FFF', R: 'FRF', B: 'FFF', L: 'FFF'},
-      rotation: app.Rotation._0
+      faces: {T: 'FFF', R: 'FRF', B: 'FFF', L: 'FFF'}
     },
     intersect_road: {
       class: 'intersect-road', count: 1,
-      faces: {T: 'FRF', R: 'FRF', B: 'FRF', L: 'FRF'},
-      rotation: app.Rotation._0
+      faces: {T: 'FRF', R: 'FRF', B: 'FRF', L: 'FRF'}
     },
     curved_road: {
       class: 'curved-road', count: 9,
-      faces: {T: 'FRF', R: 'FRF', B: 'FFF', L: 'FFF'},
-      rotation: app.Rotation._0
+      faces: {T: 'FRF', R: 'FRF', B: 'FFF', L: 'FFF'}
     },
     straight_road: {
       class: 'straight-road', count: 8,
-      faces: {T: 'FFF', R: 'FRF', B: 'FFF', L: 'FRF'},
-      rotation: app.Rotation._0
+      faces: {T: 'FFF', R: 'FRF', B: 'FFF', L: 'FRF'}
     },
     t_road: {
       class: 't-road', count: 4,
-      faces: {T: 'FRF', R: 'FRF', B: 'FRF', L: 'FFF'},
-      rotation: app.Rotation._0
+      faces: {T: 'FRF', R: 'FRF', B: 'FRF', L: 'FFF'}
     },
     four_face_castle: {
       class: 'four-face-castle', count: 1,
-      faces: {T: 'CCC', R: 'CCC', B: 'CCC', L: 'CCC'},
-      rotation: app.Rotation._0
+      faces: {T: 'CCC', R: 'CCC', B: 'CCC', L: 'CCC'}
     },
     three_face_castle: {
       class: 'three-face-castle', count: 4,
-      faces: {T: 'CCC', R: 'FFF', B: 'CCC', L: 'CCC'},
-      rotation: app.Rotation._0
+      faces: {T: 'CCC', R: 'FFF', B: 'CCC', L: 'CCC'}
     },
     three_face_castle_with_road: {
       class: 'three-face-castle-with-road', count: 3,
-      faces: {T: 'CCC', R: 'FRF', B: 'CCC', L: 'CCC'},
-      rotation: app.Rotation._0
+      faces: {T: 'CCC', R: 'FRF', B: 'CCC', L: 'CCC'}
     },
     one_face_adjacent_separate_castles: {
       class: 'one-face-adjacent-separate-castles', count: 2,
-      faces: {T: 'CCC', R: 'FFF', B: 'FFF', L: 'CCC'},
-      rotation: app.Rotation._0
+      faces: {T: 'CCC', R: 'FFF', B: 'FFF', L: 'CCC'}
     },
     one_face_castle: {
       class: 'one-face-castle', count: 5,
-      faces: {T: 'FFF', R: 'FFF', B: 'FFF', L: 'CCC'},
-      rotation: app.Rotation._0
+      faces: {T: 'FFF', R: 'FFF', B: 'FFF', L: 'CCC'}
     },
     one_face_castle_with_straight_road: {
       class: 'one-face-castle-with-straight-road',
       count: 4,
-      faces: {T: 'FRF', R: 'FFF', B: 'FRF', L: 'CCC'},
-      rotation: app.Rotation._0
+      faces: {
+        T: {face: 'FRF', segments: [1, 2, 3]},
+        R: {face: 'FFF', segments: [3]},
+        B: {face: 'FRF', segments: [1, 2, 3]},
+        L: {face: 'CCC', segments: [4]}
+      },
+      segments: {1: {type: 'F'}, 2: {type: 'R'}, 3: {type: 'F'}, 4: {type: 'C'}}
     },
     one_face_castle_with_curved_road_1: {
       class: 'one-face-castle-with-curved-road-1', count: 3,
-      faces: {T: 'FRF', R: 'FRF', B: 'FFF', L: 'CCC'},
-      rotation: app.Rotation._0
+      faces: {T: 'FRF', R: 'FRF', B: 'FFF', L: 'CCC'}
     },
     one_face_castle_with_curved_road_2: {
       class: 'one-face-castle-with-curved-road-2', count: 3,
-      faces: {T: 'FFF', R: 'FRF', B: 'FRF', L: 'CCC'},
-      rotation: app.Rotation._0
+      faces: {T: 'FFF', R: 'FRF', B: 'FRF', L: 'CCC'}
     },
     two_face_opposite_castle: {
       class: 'two-face-opposite-castle', count: 3,
-      faces: {T: 'CCC', R: 'FFF', B: 'CCC', L: 'FFF'},
-      rotation: app.Rotation._0
+      faces: {T: 'CCC', R: 'FFF', B: 'CCC', L: 'FFF'}
     },
     two_face_adjacent_castle_with_curved_road: {
       class: 'two-face-adjacent-castle-with-curved-road', count: 5,
-      faces: {T: 'FRF', R: 'FRF', B: 'CCC', L: 'CCC'},
-      rotation: app.Rotation._0
+      faces: {T: 'FRF', R: 'FRF', B: 'CCC', L: 'CCC'}
     }
   };
   app.gameView = new app.GameView();
--- models/game.js
+++ models/game.js
@@ -1,5 +1,67 @@
 var app = app || {};
 (function () {
   'use strict';
-  app.Game = Backbone.Model.extend({});
+  app.Game = Backbone.Model.extend({
+    defaults: {
+      availableTiles: undefined,
+      segments: undefined
+    },
+    initialize: function () {
+
+      debugger;
+
+
+      this.availableTiles = {}
+      this.segments = {}
+
+
+      var tempPlayableTiles = new Array();
+      _.each(app.PlayableTiles, function (playableTile) {
+
+        for (var i = 0; i < playableTile.count; i++) {
+          var clonedPlayableTile = _.clone(playableTile);
+
+          if (clonedPlayableTile.class === 'one-face-castle-with-straight-road') {
+            clonedPlayableTile.segments = {};
+            var j = 1;
+            _.each(playableTile.segments, function(segment) {
+              clonedPlayableTile.segments[j] = _.clone(segment);
+              j++;
+            });
+
+            debugger;
+            //
+            //class: 'one-face-castle-with-straight-road',
+            //    count: 4,
+            //    faces: {
+            //    T: {face: 'FRF', segment: [1, 2, 3]},
+            //    R: {face: 'FFF', segment: [3]},
+            //    B: {face: 'FRF', segment: [1, 2, 3]},
+            //    L: {face: 'CCC', segment: [4]}
+            //  },
+            //  segments: {1: {type: 'F'}, 2: {type: 'R'}, 3: {type: 'F'}, 4: {type: 'C'}}
+            //
+
+            _.each(app.NeighborDirection, function (dir) {
+
+              debugger;
+              _.each(clonedPlayableTile.faces[dir.dir.name].segments, function(segmentIndex) {
+                var segment = clonedPlayableTile.segments[segmentIndex];
+                if(!segment.id) {
+                  segment.id = app.SEGMENT_SEQ_NUM++
+                  this.segments[segment.id] = segment;
+                }
+              }, this);
+
+
+
+            }, this);
+          }
+          clonedPlayableTile.faces = _.clone(clonedPlayableTile.faces);
+          tempPlayableTiles.push(clonedPlayableTile);
+        }
+      }, this);
+
+    }
+  });
 })();
