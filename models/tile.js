42cc43899f77200afaf99e9e870ece3ebb1e8002
philippannenko <philippannenko@gmail.com>
2016-02-11 22:33:05 -0500

    Add javascript unit tests. Whoa. Move model like behavior from view to model.

--- models/tile.js
+++ models/tile.js
@@ -2,15 +2,58 @@ var app = app || {};
 (function () {
   'use strict';
   app.Tile = Backbone.Model.extend({
-    defaults: function() {
+    defaults: function () {
       return {
-        'id': -1,
-        'adjacentNeighbors': {},
-        'class': "",
-        'faces': {},
-        'segments': [],
-        'state': app.TileState.undefined,
-        'rotation': app.Rotation._0}
+        id: -1,
+        adjacentNeighbors: {},
+        class: "",
+        faces: {},
+        segments: [],
+        state: app.TileState.undefined,
+        rotation: app.Rotation._0
+      }
+    },
+
+    rotate: function (rotation) {
+      rotation = rotation.currentTarget.id;
+      this.set('rotation', this.get('rotation')[rotation]);
+      var faces = this.get('faces');
+      var temp = faces['T'];
+      if (rotation === 'CW') {
+        faces['T'] = faces['L'];
+        faces['L'] = faces['B'];
+        faces['B'] = faces['R'];
+        faces['R'] = temp;
+      } else if (rotation === 'CCW') {
+        faces['T'] = faces['R'];
+        faces['R'] = faces['B'];
+        faces['B'] = faces['L'];
+        faces['L'] = temp;
+      }
+    },
+
+    compareTileToCurrentTurnTile: function(tile) {
+      var foundConflictingNeighborFace = _.find(app.NeighborDirection, function (dir, key) {
+        var adjacentNeighbor = tile.get('adjacentNeighbors')[key];
+        if (adjacentNeighbor && adjacentNeighbor.get('state') === app.TileState.occupied) {
+          var oppositeDir = dir.dir.opposite.name;
+          var neighborFace = adjacentNeighbor.get('faces')[oppositeDir];
+          var playableTileFace = this.get('faces')[key];
+          return playableTileFace.face !== neighborFace.face;
+        } else {
+          return false;
+        }
+      }, this);
+      if (!foundConflictingNeighborFace) {
+        // assign this models tile info with the tile that was passed into this method
+        tile.set({
+          class: this.get('class'),
+          faces: this.get('faces'),
+          segments: this.get('segments'),
+          rotation: this.get('rotation'),
+          state: app.TileState.occupied
+        });
+      }
 
     }
   });
